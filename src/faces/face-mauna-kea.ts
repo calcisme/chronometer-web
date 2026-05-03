ecccd6b2afb515e22d2d46ccc828e55f8c6d5d18
Steve Pucci <spucci@emeraldsquoia.com>
2026-05-03 15:53:41 -0700

    Use new EOT dial with ticks on MK

--- src/faces/face-mauna-kea.ts
+++ src/faces/face-mauna-kea.ts
@@ -3,7 +3,6 @@
  */
 import xml from '../watch/assets/mauna-kea/MaunaKea-I.xml';
 import faceUrl from '../watch/assets/mauna-kea/astro-face-4x.png';
-import eotUrl from '../watch/assets/mauna-kea/EOT-4x.png';
 import morningUrl from '../watch/assets/mauna-kea/morningHD-4x.png';
 import eveningUrl from '../watch/assets/mauna-kea/eveningHD-4x.png';
 import zodiacUrl from '../watch/assets/mauna-kea/zodiacWheel-4x.png';
@@ -16,7 +15,6 @@ window.ChronometerFaces.push({
     xml,
     images: {
         'astro-face.png':    { dataUrl: faceUrl, scale: 0.25 },
-        'EOT.png':           { dataUrl: eotUrl, scale: 0.25 },
         'morningHD.png':     { dataUrl: morningUrl, scale: 0.25 },
         'eveningHD.png':     { dataUrl: eveningUrl, scale: 0.25 },
         'zodiacWheel.png':   { dataUrl: zodiacUrl, scale: 0.25 },
